d75d8ffe4452ea45bf41c45f555bbdb643985fed
Anastassiya <imperator-taya@mail.ru>
2019-06-26 20:17:12 +0600

    Edit code accaunt

--- app/src/main/java/com/example/bsfragments/accaunt.java
+++ app/src/main/java/com/example/bsfragments/accaunt.java
@@ -9,40 +9,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class accaunt extends Fragment {
     private TextView user_hello;
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         user_hello = (TextView) getView().findViewById(R.id.hello_user);
-        mAuth = FirebaseAuth.getInstance();
-
         return inflater.inflate(R.layout.fragment_accaunt, null);
     }
 
     @Override
     public void onResume() {
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    user_hello.setText("Привет тебе, " +getArguments().getString("user_login") + "!\nНиже показаны твои действия в приложении");
-                } else {
-                    Toast.makeText(getActivity(), "Вам необходимо пройти авторизацию", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(getActivity(), LoginActivity.class);
-                    startActivity(intent);
-                }
-
-            }
-        };
+        user_hello.setText("Привет тебе, " +getArguments().getString("user_login") + "!\nНиже показаны твои действия в приложении");
         super.onResume();
     }
 }
